58b69397f1c3870e8a6c14781d5fce7eb693b711
LoganXav <sogbesansegun22@gmail.com>
2023-08-18 09:45:17 +0100

    appProtected UI

--- frontend/src/features/auth/AuthScaffold.jsx
+++ frontend/src/features/auth/AuthScaffold.jsx
@@ -6,7 +6,7 @@ import { clsx } from "clsx";
 function AuthScaffold(props) {
   return (
     <div className="min-h-screen flex bg-mui-primary-lightAlt">
-      <div className="w-[90%] md:w-[80%] my-24 sm:my-12 flex sm:flex-col xl:flex-row mx-auto overflow-hidden rounded-sm" >
+      <div className="w-[95%] md:w-[80%] my-24 sm:my-12 flex sm:flex-col xl:flex-row mx-auto overflow-hidden rounded-sm" >
         <div className="xl:w-[45%] hidden sm:flex flex-1">
           <div className="w-full flex flex-col items-center justify-between text-center p-10 bg-mui-secondary-main rounded-sm">
             <div>
@@ -16,7 +16,7 @@ function AuthScaffold(props) {
             <Typography variant="subtitle1" className="text-mui-secondary-contrastText" >2023 Steex. Crafted with love by Logan</Typography>
           </div>
         </div>
-        <Paper className="w-full xl:w-[55%] flex flex-col items-center justify-center text-center p-6 sm:py-12 xl:p-0">
+        <Paper className="w-full xl:w-[55%] flex flex-col items-center justify-center text-center  sm:py-12 xl:p-0">
             <div className="w-full sm:max-w-3xl xl:max-w-md">{props.children}</div>
         </Paper>
       </div>
