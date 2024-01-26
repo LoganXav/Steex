a0faf5ce6f7433f4254950bf53dc051460572896
Timilehin Sogbesan <sogbesansegun22@gmail.com>
2024-01-26 05:14:49 +0100

    added logger, mongoose and exress loaders

---
+++ backend/src/app.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import config from './config';
+import Logger from './loaders/logger';
+import loaders from './loaders';
+
+async function startServer() {
+  const app = express();
+
+  await loaders({ expressApp: app });
+
+  app.listen(config.port, () => {
+    Logger.info(
+      `
+    ################################################
+    🛡️  Server listening on port: ${config.port} 🛡️
+    ################################################
+    `,
+    ).on('error', err => {
+      Logger.error(err);
+      process.exit(1);
+    });
+  });
+}
+startServer();
---
+++ backend/src/loaders/express.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+export default ({ app }: { app: express.Application }) => {
+  /**
+   * Health Check endpoints
+   * @TODO Explain why they are here
+   */
+  app.get('/status', (req, res) => {
+    res.status(200).end();
+  });
+  app.head('/status', (req, res) => {
+    res.status(200).end();
+  });
+};
